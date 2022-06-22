74c45381717f505b140a7bf90eca8d225ce3c9c7
Ashu Kumar <kumar.ashu414283@gmail.com>
2022-06-22 13:21:54 +0530

    pwa

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,24 +13,24 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
         />
-        <meta name="description" content="Description" />
-        <meta name="keywords" content="Keywords" />
+        <meta name="description" content="Instagram Reels Downloader" />
+        <meta name="keywords" content="instagram, reels, downloader" />
         <title>Instagram Reel Downloader</title>
 
         <link rel="manifest" href="/manifest.json" />
         <link
-          href="/icons/favicon-16x16.png"
+          href="/icons/icon_x48.png"
           rel="icon"
           type="image/png"
           sizes="16x16"
         />
         <link
-          href="/icons/favicon-32x32.png"
+          href="/icons/icon_x48.png"
           rel="icon"
           type="image/png"
-          sizes="32x32"
+          sizes="48x48"
         />
-        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        <link rel="apple-touch-icon" href="/favicon.png"></link>
         <meta name="theme-color" content="#317EFB" />
       </Head>
       <Component {...pageProps} />
